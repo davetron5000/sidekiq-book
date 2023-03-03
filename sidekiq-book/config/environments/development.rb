48f8a8a185ce0b148843797f8bffaa20215eaf0a
Dave Copeland <davec@naildrivin5.com>
2023-03-02 19:01:35 -0500

    add hostname used for book building

--- sidekiq-book/config/environments/development.rb
+++ sidekiq-book/config/environments/development.rb
@@ -56,6 +56,8 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.hosts << "sidekiq-book"
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
