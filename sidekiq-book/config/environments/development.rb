bca18a77f203e05ef18e89197e8c05ba89344eb2
Dave Copeland <davec@naildrivin5.com>
2023-03-28 16:45:27 -0400

    use file store so sidekiq sees the cache

--- sidekiq-book/config/environments/development.rb
+++ sidekiq-book/config/environments/development.rb
@@ -23,14 +23,15 @@ Rails.application.configure do
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
-    config.cache_store = :memory_store
+    config.cache_store = :file_store, "/tmp/rails-cache"
+
     config.public_file_server.headers = {
       "Cache-Control" => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
 
-    config.cache_store = :memory_store
+    config.cache_store = :file_store, "/tmp/rails-cache"
   end
 
   # Don't care if the mailer can't send.
