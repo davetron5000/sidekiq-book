4d74618d0051403b0a671b86f50e321169f8182e
Dave Copeland <davec@naildrivin5.com>
2023-04-10 09:40:58 -0400

    add wrapper for email search

--- sidekiq-book/app/services/email_service_wrapper.rb
+++ sidekiq-book/app/services/email_service_wrapper.rb
@@ -21,12 +21,41 @@ class EmailServiceWrapper < BaseServiceWrapper
     end
   end
 
+  def search_emails(email, template_id)
+    uri = URI(@url + "/emails")
+    params = { email: email, template_id: template_id }
+    uri.query = URI.encode_www_form(params)
+    http_response = @net_http.get_response(uri,headers)
+    if http_response.code == "200"
+      JSON.parse(http_response.body).map { |email|
+        ParsedEmail.new(
+          to: email["to"],
+          template_id: email["template_id"],
+          email_id: email["email_id"],
+          template_data: email["template_data"],
+        )
+      }
+    else
+      raise_error!(http_response)
+    end
+  end
+
   class Result
     attr_reader :email_id
     def initialize(email_id)
       @email_id = email_id
     end
     def success? = true
+  end
+
+  class ParsedEmail
+    attr_reader :to, :template_id, :email_id, :template_data
+    def initialize(to:, template_id:, email_id:, template_data:)
+      @to            = to
+      @template_id   = template_id
+      @email_id      = email_id
+      @template_data = template_data
+    end
   end
 
 end
