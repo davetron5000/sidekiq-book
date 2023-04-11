d6108ab5418a7430400dc0b8aaa6a1c5a210257e
Dave Copeland <davec@naildrivin5.com>
2023-04-11 12:14:59 -0400

    use fixtures

--- sidekiq-book/test/services/order_creator_test.rb
+++ sidekiq-book/test/services/order_creator_test.rb
@@ -6,49 +6,31 @@ class OrderCreatorTest < ActiveSupport::TestCase
   end
 
   test "create_order charge declines" do
-    product = Product.create!(
-      name: "Test Product",
-      price_cents: 99_99, # special value that triggers a decline
-      quantity_remaining: 10
-    )
-    user = User.create!(
-      email: "pat@example.com",
-      payment_method_id: SecureRandom.uuid,
-    )
     order = Order.create!(
       email: "pat@example.com",
       address: "123 Main St",
       quantity: 1,
-      product: product,
-      user: user,
+      product: products(:priced_for_decline),
+      user: users(:pat),
     )
     resulting_order = @order_creator.create_order(order)
-    assert_equal order, resulting_order
+    assert_equal order, resulting_order, "should return the same order"
     refute resulting_order.charge_successful
     assert_equal "Insufficient funds", resulting_order.charge_decline_reason
     assert_nil resulting_order.charge_id
   end
 
   test "create_order charge succeeds" do
-    product = Product.create!(
-      name: "Test Product",
-      price_cents: 100,
-      quantity_remaining: 10
-    )
-    user = User.create!(
-      email: "pat@example.com",
-      payment_method_id: SecureRandom.uuid,
-    )
     order = Order.create!(
       email: "pat@example.com",
       address: "123 Main St",
       quantity: 2,
-      product: product,
-      user: user,
+      product: products(:stembolt),
+      user: users(:pat),
     )
 
     resulting_order = @order_creator.create_order(order)
-    assert_equal order, resulting_order
+    assert_equal order, resulting_order, "should return the same order"
     assert resulting_order.charge_successful
     assert_nil resulting_order.charge_decline_reason
     refute_nil resulting_order.charge_id
